d6897105c543d50e7c347238c4cf67b501bde53d
Priyanka-2808 <87664708+Priyanka-2808@users.noreply.github.com>
2023-08-10 15:08:28 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+//    Pizza basePizza = new Pizza(true);
+//        basePizza.addExtraToppings();
+//        basePizza.addExtraCheese();
+//        basePizza.takeAway();
+//        basePizza.getBill();
+        DeluxePizza dp = new DeluxePizza(true);
+        dp.takeAway();
+        dp.getBill();
+    }
+}
